d2b42ecec16c6bd0378922edf46bf2b3e8aba96f
DOMIN1310 <dominik.poweski@wp.pl>
2024-06-05 19:54:52 +0200

    optimized init and new var

--- builds/init.go
+++ builds/init.go
@@ -20,159 +20,109 @@ import (
 func Search(ch chan string, property string, ctx context.Context){
 	switch property{
 	case "index.ts":
-		if req, err := http.NewRequest(
-			"GET",
-			"https://raw.githubusercontent.com/DOMIN1310/webmake/master/res/tsconfig.json",
-			nil,
-		); err != nil {
-			log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, err.Error());
-		} else {
-			if res, err := http.DefaultClient.Do(req); err != nil {
-				log.Fatalf("%v:%v%v\n", v.ERROR, v.ERROR, err.Error());
-			} else {
-				if buffer, err := io.ReadAll(res.Body); err != nil {
-					log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, err.Error());
-				} else {
-					if err := os.Mkdir("ts", 0766); err != nil {
-						log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, "could not create ts directory");
-					} else {
-						if err := os.WriteFile(path.Join("tsconfig.json"), buffer, 0744); err != nil{
-							log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, "could not create and overwrite ts/tsconfig.json");
-						} else {
-							if err := os.WriteFile("ts/index.ts", []byte{}, 0744); err != nil{
-								log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, err.Error())
-							} else {
-								if ctx.Err() != nil{
-									log.Printf("%v:%v%v\n", v.WARN, v.RESET, "UNABLE TO REACH THAT CODE!");
-								} else {
-									ch<- property;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+		var url string = "https://raw.githubusercontent.com/DOMIN1310/webmake/master/res/tsconfig.json";
+		PrepareComponent(ch, "ts", map[string]string{property: ""}, ctx);
+		PrepareComponent(ch, "./ts", GetURLBody(url, ctx, "GET", property), ctx);
 	case "tailwindutils.css":
-		if _, err := Cmd(func() *exec.Cmd {
+		if err := Cmd(func() *exec.Cmd {
 			return exec.Command("pnpm", "install", "-D", "tailwindcss")
 		}()); err != nil{
 			log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, err.Error());
 		} else {
-			log.Printf("%v:%v%v\n", v.SUCCESS, v.RESET, "successfully initialized tailwindcss");
+			log.Printf("%v:%v%v\n", v.INIT, v.RESET, "successfully initialized tailwindcss");
 		}
-		if req, err := http.NewRequest(
-				"GET", 
-				"https://raw.githubusercontent.com/DOMIN1310/webmake/master/res/tailwind.config.js",
-				nil,
-			); err != nil{
-				log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, err.Error());
-			} else {
-				if res, err := http.DefaultClient.Do(req); err != nil {
-					log.Fatalf("%v:%v%v\n", v.ERROR, v.ERROR, err.Error());
-				} else {
-					if buffer, err := io.ReadAll(res.Body); err != nil {
-						log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, err.Error());
-					} else {
-						if err := os.WriteFile("tailwind.config.js", buffer, 0744); err != nil{
-							log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, "could not create and overwrite tailwind.config.js");
-						} else {
-							if ctx.Err() != nil{
-								log.Printf("%v:%v%v\n", v.WARN, v.RESET, "UNABLE TO REACH THAT CODE!");
-							} else {
-								if err := os.Mkdir("css", 0766); err != nil {
-									log.Printf("%v:%v%v\n", v.WARN, v.RESET, "unable to create css directory!");
-								} else {
-									if err := os.WriteFile(
-										"css/tailwind.css",
-										[]byte("@tailwind base;\n@tailwind components;\n@tailwind utilities;"),
-										0744,
-									); err != nil {
-										log.Printf("%v:%v%v\n", v.WARN, v.RESET, "unable to create and overwrite css/tailwind.css");
-									} else {
-										ch<-"tailwind";
-									}
-								}
-							}
-						}
-					}
-				}
-			}
+		var url string = "https://raw.githubusercontent.com/DOMIN1310/webmake/master/res/tailwind.config.js";
+		PrepareComponent(ch, "./", GetURLBody(url, ctx, "GET", property), ctx);
+		PrepareComponent(ch, "css", map[string]string{
+			"tailwind.css": "@tailwind base;\n@tailwind components;\n@tailwind utilities;",
+		}, ctx);
 	case "main.scss":
-		if _, err := Cmd(func() *exec.Cmd{
+		if err := Cmd(func() *exec.Cmd{
 			return exec.Command("pnpm", "install", "sass", "--save-dev");
 		}()); err != nil {
 			log.Printf("%v:%v%v\n", v.WARN, v.RESET, err.Error());
 		} else {
-			log.Printf("%v:%v%v\n", v.SUCCESS, v.RESET, "successfully initialized sass");
-			if err := os.Mkdir("sass", 0766); err != nil {
-				log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, "could not create sass directory");
-			} else {
-				if _, err := os.Create("sass/main.scss"); err != nil {
-					log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, "error while creating main.scss");
-				} else {
-					if ctx.Err() != nil{
-						log.Printf("%v:%v%v\n", v.WARN, v.RESET, "UNABLE TO REACH THAT CODE!");
-					} else {
-						ch<-property;
-					}
-				}
-			}
+			log.Printf("%v:%v%v\n", v.INIT, v.RESET, "successfully initialized sass");
+			PrepareComponent(ch, "sass", map[string]string{
+				property: "",
+			}, ctx);
 		}
 	default:
-		if property == "index.html" || property == "index.php"{
-			if err := os.Mkdir("public", 0766); err != nil{
-				log.Printf("%v:%v%v\n", v.WARN, v.RESET, "unable to create public directory");
+		PrepareComponent(ch, func () string {
+			if property == "main.css" {
+				return "css";
+			} else if property == "index.js" {
+				return "js";
 			} else {
-				log.Printf("%v:%v%v\n", v.SUCCESS, v.RESET, "successfully created public directory");
+				return "public";
+			}
+		}(), func () map[string]string {
+			if property == "index.html" || property == "index.php" {
 				var url string = fmt.Sprintf("https://raw.githubusercontent.com/DOMIN1310/webmake/master/res/%v", property);
-				if req, err := http.NewRequest("GET", url, nil); err != nil {
-					log.Printf("%v:%v%v\n", v.WARN, v.RESET, err.Error());
-				} else {
-					if res, err := http.DefaultClient.Do(req); err != nil{
-						log.Printf("%v:%v%v\n", v.WARN, v.RESET, err.Error());
-					} else {
-						if buffer, err := io.ReadAll(res.Body); err != nil{
-							log.Printf("%v:%v%v\n", v.WARN, v.RESET, err.Error());
-						} else {
-							if err := os.WriteFile(path.Join("public", property), buffer, 0744); err != nil {
-								log.Printf("%v:%v%v\n", v.WARN, v.RESET, "could not create or overwrite " + property);
-							} else {
-								if ctx.Err() != nil {
-									log.Printf("%v:%v%v\n", v.WARN, v.RESET, "UNABLE TO REACH THAT CODE!");
-								} else {
-									ch <- property;
-								}
-							}
-						}
-					}
-				}
-			}
-		} else if property == "index.js" {
-			if err := os.Mkdir("js", 0766); err != nil {
-				log.Printf("%v:%v%v\n",v.WARN, v.RESET, "could not create js directory")
+				return GetURLBody(url, ctx, "GET", property);
 			} else {
-				if _, err := os.Create("js/index.js"); err != nil {
-					log.Printf("%v:%v%v\n", v.WARN, v.RESET, "could not create index.js");
-				}
+				return map[string]string{property: ""};
 			}
-		} else if property == "main.css" {
-			if err := os.Mkdir("css", 0766); err != nil {
-				log.Printf("%v:%v%v\n",v.WARN, v.RESET, "could not create css directory")
+		}(), ctx);
+	}
+}
+
+func GetURLBody(url string, ctx context.Context, method string, file string) map[string]string {
+	if req, err := http.NewRequestWithContext(ctx, method, url, nil); err != nil {
+		log.Printf("%v:%v%v\n", v.WARN, v.RESET, "unable to get any request within context and destiny url");
+		return map[string]string{file: ""};	
+	} else if ctx.Err() != nil {
+		fmt.Printf("%v:%v%v\n", v.WARN, v.RESET, "unable to complete request: context error")
+		return map[string]string{file: ""};
+	}else {
+		if res, err := http.DefaultClient.Do(req); err != nil {
+			log.Printf("%v:%v%v\n", v.WARN, v.RESET, "unable to complete request with proper response");
+			return map[string]string{file: ""};
+		} else {
+			if buffer, err := io.ReadAll(res.Body); err != nil {
+				log.Printf("%v:%v%v\n", v.WARN, v.RESET, "unable to read response body!");
+				return map[string]string{file: ""};
 			} else {
-				if _, err := os.Create("css/main.css"); err != nil {
-					log.Printf("%v:%v%v\n", v.WARN, v.RESET, "could not create main.css");
+				return map[string]string{
+					file: string(buffer),
 				}
-			}
+			}						
 		}
 	}
 }
 
-func Cmd(f *exec.Cmd) ([]byte, error) {
-	if output, err := f.Output(); err != nil {
-		return nil, errors.New("incorrect command");
+func PrepareComponent(delch chan string, dirName string, files map[string]string, ctx context.Context) {
+	if dirName != "./" {
+		if _, err := os.Stat(dirName); os.IsNotExist(err) {
+			if err := os.Mkdir(dirName, 0755); err != nil {
+				log.Printf("%v:%v%v\n", v.WARN, v.RESET, "unable to create " + dirName + " directory");
+			}
+		} else if err != nil {
+			log.Printf("%v:%v%v\n", v.ERROR, v.RESET, "unable to check if directory exists or not!");
+			return;
+		} else if ctx.Err() != nil {
+			log.Printf("%v:%v%v", v.ERROR, v.RESET, "context error!! unable to reach the code further!");
+		} else {
+			log.Printf("%v:%v%v\n", v.WARN, v.RESET, "directory already exists");
+		}
+	}
+	for fileName, content := range files {
+		if err := os.WriteFile(path.Join(dirName, fileName), []byte(content), 0755); err != nil {
+			log.Printf("%v:%v%v\n", v.WARN, v.RESET, "could not create " + fileName);
+		} else {
+			if delch != nil {
+				delch<-fileName;
+			}
+		}
+	}
+
+}
+
+func Cmd(f *exec.Cmd) error {
+	if _, err := f.Output(); err != nil {
+		return errors.New("incorrect command");
 	} else {
-		return output, nil;
+		return nil;
 	}
 }
 
@@ -192,28 +142,38 @@ func createWeb() error{
 				var chfile chan string = make(chan string, 96);
 				var ctx, deadline = context.WithDeadline(context.Background(), time.Now().Add(10*time.Second));
 				defer deadline();
-				if _, err := Cmd(func() *exec.Cmd{
+				if err := Cmd(func() *exec.Cmd{
 					return exec.Command("pnpm", "init");
 				}()); err != nil {
 					log.Printf("%v:%v%v\n", v.WARN, v.RESET, err.Error());
 				} else {
-					log.Printf("%v:%v%v\n", v.SUCCESS, v.RESET, "successfully initialized package.json");
+					log.Printf("%v:%v%v\n", v.INIT, v.RESET, "successfully initialized package.json");
+				}
+				if conf.Git {
+					if err := Cmd(func () *exec.Cmd {
+						return exec.Command("git", "init");
+					}()); err != nil {
+						log.Printf("%v:%v%v\n", v.ERROR, v.RESET, "unable to initialize git ensure u have git installed");
+					} else {
+						log.Printf("%v:%v%v\n", v.INIT, v.RESET,"successfully initialized git!")
+					}
 				}
 				go Search(chfile, conf.Findex, ctx);
 				go Search(chfile, conf.Styleindex, ctx);
 				go Search(chfile, conf.Tmplindex, ctx);
-				for {
+				for i := 0; i <= 3; i++{
 					select {
 					case <- ctx.Done():
-						log.Printf("%v:%v%v\n", v.WARN, v.RESET, "contexted has finished");
+						log.Printf("%v:%v%v\n", v.FINISHED, v.RESET, "context has finished");
 						return nil;
 					case msg := <- chfile:
-						log.Printf("%v:%v%v\n", v.SUCCESS, v.RESET, msg);
+						log.Printf("%v:%v%v\n", v.SUCCESS, v.RESET, fmt.Sprintf("successfully created %v!", msg));
 					}
 				}
 			}
 		}
 	}
+	return nil;
 }
 
 func InitPackage() {
@@ -231,7 +191,7 @@ func InitPackage() {
 	if flang != "ts" && flang != "js" {
 		log.Fatalf("%v:%v%v", v.ERROR, v.RESET, "invalid option choose either js or ts");
 	}
-	fmt.Println("stylesheet [base (css), scss, tailwind]: ");
+	fmt.Println("stylesheet [basic (css), scss, tailwind]: ");
 		//styles
 	fmt.Scan(&style);
 	if style != "basic" && style != "scss" && style != "tailwind"{
@@ -249,8 +209,7 @@ func InitPackage() {
 	if b, e := strconv.ParseBool(temp); e != nil {
 		git = b;
 	}
-	fmt.Println("app directory name: ");
-	if style == "css" {
+	if style == "basic" {
 		style = "main.css";
 	} else if style == "scss" {
 		style = "main.scss";	
@@ -270,14 +229,6 @@ func InitPackage() {
 	} else {
 		log.Printf("%v:%v%v\n", v.SUCCESS, v.RESET, "Marshaling was successful!");
 	}
-	if err = os.WriteFile("wb-package.json", buffer, 0666); err != nil{
-		log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, err.Error());
-	} else {
-		log.Printf("%v:%v%v\n", v.SUCCESS, v.RESET, "wb-package overwritten successfully!")
-	}
-	if err = createWeb(); err != nil{
-		log.Fatalf("%v:%v%v\n", v.ERROR, v.RESET, err.Error());
-	} else {
-		log.Printf("%v:%v%v\n", v.SUCCESS, v.RESET, "CREATION COMPLETED SUCCESSFULLY!")
-	}
+	PrepareComponent(nil, "./", map[string]string{"wb-package.json": string(buffer)}, nil)
+	createWeb();
 }
--- vars/vars.go
+++ vars/vars.go
@@ -8,8 +8,10 @@ type Template struct{
 }
 
 var (
+	FINISHED string = "\033[34m[FINISHED]";
 	ERROR string = "\033[31m[ERROR]";
 	WARN string = "\033[33m[WARN]";
-	SUCCESS string = "\033[32m[SUCCESS]"
-	RESET string = "\033[0m"
+	SUCCESS string = "\033[32m[SUCCESS]";
+	RESET string = "\033[0m";
+	INIT string = "\033[36m[INIT]";
 )
